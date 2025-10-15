16738d2bc4ea4b17fa50b507f62ac027e9a8502f
shajid shahriar <shajidsr12@gmail.com>
2025-10-15 14:04:18 +0600

    refactor the code with classes and constructors

--- js/script.js
+++ js/script.js
@@ -1,36 +1,41 @@
-const global = {
-  currentPage: window.location.pathname,
-};
-
-//init app
-function highlightActiveLink() {
-  const links = document.querySelectorAll(".nav-link");
-  links.forEach((link) => {
-    if (link.getAttribute("href") === global.currentPage) {
-      link.classList.add("active");
-    }
-  });
-}
-function init() {
-  switch (global.currentPage) {
-    case "/":
-    case "/index.html":
-      console.log("home");
-      break;
-    case "/shows.html":
-      console.log("shows");
-      break;
-    case "/movie-details.html":
-      console.log("movies ");
-      break;
-    case "/tv-details.html":
-      console.log("tv");
-      break;
-    case "/search.html":
-      console.log("search");
-      break;
+class App {
+  constructor() {
+    this.currentPage = window.location.pathname;
+    this.init();
   }
-  highlightActiveLink()
+
+  init() {
+    switch (this.currentPage) {
+      case "/":
+      case "/index.html":
+        console.log("home");
+        break;
+      case "/shows.html":
+        console.log("shows");
+        break;
+      case "/movie-details.html":
+        console.log("movies ");
+        break;
+      case "/tv-details.html":
+        console.log("tv");
+        break;
+      case "/search.html":
+        console.log("search");
+        break;
+    }
+    this.highlightActiveLink();
+  }
+
+  highlightActiveLink() {
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach((link) => {
+      if (link.getAttribute("href") === this.currentPage) {
+        link.classList.add("active");
+      }
+    });
+  }
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new App();
+});
