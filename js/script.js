0bf6db9e396c58993f3f7056b28c75c36f5c9267
shajid shahriar <shajidsr12@gmail.com>
2025-10-20 22:56:39 +0600

    add method for displaying movie details page

--- js/script.js
+++ js/script.js
@@ -48,10 +48,11 @@ class App {
     console.log(results);
 
     results.forEach((movie) => {
+      const movieID = movie.id
       const div = document.createElement("div");
       div.classList.add("card");
       div.innerHTML = `
-            <a href="movie-details.html?id=1">
+            <a href="movie-details.html?id=${movieID}">
               ${
                 movie.poster_path
                   ? `<img
@@ -113,8 +114,59 @@ class App {
     });
   }
   async displayMovieDetails(){
-    const movie = await this._fetchAPIData("movie/1156594")
+    const urlParams = new URLSearchParams(window.location.search)
+    const movieId = urlParams.get('id')
+    console.log(movieId)
+
+
+    const movie = await this._fetchAPIData(`movie/${movieId}`)
     console.log(movie)
+    const detailsContainer = document.querySelector(".movie-details")
+    detailsContainer.innerHTML = `
+        <div class="details-top">
+          <div>
+          ${movie.poster_path ?
+
+            `<img
+                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.original_title}"
+                />` :`<img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="${movie.original_title}"
+                />`
+                }  
+          </div>
+          <div>
+            <h2>${movie.original_title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              8 / 10
+            </p>
+            <p class="text-muted">Release Date:${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> ${movie.budget} </li>
+            <li><span class="text-secondary">Revenue:</span> ${movie.revenue}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes </li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map(company => ` ${company.name}`)}</div>
+        </div>
+      `
   }
 
   //private methods
